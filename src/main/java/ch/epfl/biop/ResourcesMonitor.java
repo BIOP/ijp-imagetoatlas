b9a99f968df4eacae45131bcfaf157ff8d211077
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-02-01 12:50:49 +0100

    fixes resource monitor

--- src/main/java/ch/epfl/biop/ResourcesMonitor.java
+++ src/main/java/ch/epfl/biop/ResourcesMonitor.java
@@ -1,16 +1,22 @@
 package ch.epfl.biop;
 
-import ch.epfl.biop.atlas.aligner.MultiSlicePositioner;
 import com.sun.management.OperatingSystemMXBean;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HardwareAbstractionLayer;
 
+//import java.lang.management.OperatingSystemMXBean;
+
+import javax.management.MBeanServerConnection;
 import javax.swing.*;
 import java.awt.*;
 import java.lang.management.ManagementFactory;
+//import java.lang.management.ManagementFactory;
 
 public class ResourcesMonitor extends JPanel {
 
-    final JLabel cpuLabelJava;
-    final JProgressBar cpuBarJava;
+    //final JLabel cpuLabelJava;
+    //final JProgressBar cpuBarJava;
 
     final JLabel cpuLabelSystem;
     final JProgressBar cpuBarSystem;
@@ -19,16 +25,15 @@ public class ResourcesMonitor extends JPanel {
     final JProgressBar memBar;
 
     final JButton gcButton;
-    //final JButton clearActionStack;
 
-    public ResourcesMonitor(){//MultiSlicePositioner mp) {
-        this.setLayout(new GridLayout(8,1));
+    public ResourcesMonitor() {
+        this.setLayout(new GridLayout(8, 1));
 
         cpuLabelSystem = new JLabel("CPU Usage - System (%)");
         cpuBarSystem = new JProgressBar();
 
-        cpuLabelJava = new JLabel("CPU Usage - Java (%)");
-        cpuBarJava = new JProgressBar();
+        //cpuLabelJava = new JLabel("CPU Usage - Java (%)");
+        //cpuBarJava = new JProgressBar();
 
         memLabel = new JLabel("Mem (/)");
         memBar = new JProgressBar();
@@ -38,43 +43,58 @@ public class ResourcesMonitor extends JPanel {
 
         this.add(cpuLabelSystem);
         this.add(cpuBarSystem);
-        this.add(cpuLabelJava);
-        this.add(cpuBarJava);
+        //this.add(cpuLabelJava);
+        //this.add(cpuBarJava);
         this.add(memLabel);
         this.add(memBar);
         this.add(gcButton);
-        //this.add(clearActionStack);
-        gcButton.addActionListener(e -> System.gc());
-        //clearActionStack.addActionListener(e -> mp.clearUserActions());
 
-        OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(
-                OperatingSystemMXBean.class);
+        gcButton.addActionListener(e -> System.gc());
+
+        //OperatingSystemMXBean osBean =
+        //        ManagementFactory.getPlatformMXBean(
+        //                OperatingSystemMXBean.class);
 
         Thread monitor = new Thread(() -> {
-            while(true) {
+            while (true) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 
-                int loadJava = (int)(osBean.getProcessCpuLoad()*100);
-                cpuBarJava.setValue(loadJava);
-                cpuLabelJava.setText("CPU Usage - Java ("+loadJava+"%)");
+                //int loadJava = 0;//(int) (osBean.getProcessCpuLoad() * 100);
+                //cpuBarJava.setValue(loadJava);
+                //cpuLabelJava.setText("CPU Usage - Java (" + loadJava + "%)");
 
-                int loadSystem = (int)(osBean.getSystemCpuLoad()*100);
+                int loadSystem = (int) getCPU();//proc.g;//0;//(int) (osBean.getSystemCpuLoad() * 100);
                 cpuBarSystem.setValue(loadSystem);
-                cpuLabelSystem.setText("CPU Usage - System ("+loadSystem+"%)");
+                cpuLabelSystem.setText("CPU Usage - System (" + loadSystem + "%)");
 
-                double usedMemMb = ((double)Runtime.getRuntime().totalMemory() - (double)Runtime.getRuntime().freeMemory())/(1024*1024);
-                double memTotalMb = ((double)Runtime.getRuntime().totalMemory())/(1024*1024);
+                double usedMemMb = ((double) Runtime.getRuntime().totalMemory() - (double) Runtime.getRuntime().freeMemory()) / (1024 * 1024);
+                double memTotalMb = ((double) Runtime.getRuntime().totalMemory()) / (1024 * 1024);
 
-                memLabel.setText("Mem ("+((int)usedMemMb)+" Mb / "+((int) memTotalMb)+" Mb)");
-                memBar.setValue((int)(usedMemMb/memTotalMb*100));
+                memLabel.setText("Mem (" + ((int) usedMemMb) + " Mb / " + ((int) memTotalMb) + " Mb)");
+                memBar.setValue((int) (usedMemMb / memTotalMb * 100));
             }
         });
 
         monitor.start();
+
     }
 
+    private SystemInfo si = new SystemInfo();
+    private HardwareAbstractionLayer hal = si.getHardware();
+    private CentralProcessor cpu = hal.getProcessor();
+    long[] prevTicks = new long[CentralProcessor.TickType.values().length];
+
+    //private SystemInfo systemInformation = new SystemInfo();
+    //private CentralProcessor cpu = systemInformation.getHardware().getProcessor();
+
+    public double getCPU() {
+        double cpuLoad = cpu.getSystemCpuLoadBetweenTicks(prevTicks) * 100;
+        prevTicks = cpu.getSystemCpuLoadTicks();
+        //System.out.println("cpuLoad : " + cpuLoad);
+        return cpuLoad;
+    }
 }
