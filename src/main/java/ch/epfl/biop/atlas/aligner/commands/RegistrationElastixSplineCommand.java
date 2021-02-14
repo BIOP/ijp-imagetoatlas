123eb0c29fdcd214c6f8b89d6980d6fd6f81bb90
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-02-14 22:25:50 +0100

    checks number of control points in spline registration command

--- src/main/java/ch/epfl/biop/atlas/aligner/commands/RegistrationElastixSplineCommand.java
+++ src/main/java/ch/epfl/biop/atlas/aligner/commands/RegistrationElastixSplineCommand.java
@@ -7,14 +7,21 @@ import org.scijava.plugin.Plugin;
 @Plugin(type = Command.class, menuPath = "Plugins>BIOP>Atlas>Multi Image To Atlas>Align>Elastix Registration (Spline)")
 public class RegistrationElastixSplineCommand extends RegistrationCommand {
 
-    @Parameter(label = "Number of control points (along X)", min = "2")
-    int nbControlPointsX;
+    @Parameter(label = "Number of control points along X, minimum 2.", validater = "checkNumberOfPoints")
+    int nbControlPointsX = 10;
 
     @Parameter(label = "Show registration results as ImagePlus")
     boolean showIJ1Result;
 
     public void runValidated() {
         mp.registerElastixSpline(getFixedFilter(), getMovingFilter(), nbControlPointsX, showIJ1Result);
+    }
+
+    public void checkNumberOfPoints() {
+        if (nbControlPointsX<2) {
+            mp.errorMessageForUser.accept("Cannot start registration", "Number of control points too low.");
+            validationError = true;
+        }
     }
 
 }
--- src/main/java/ch/epfl/biop/atlas/aligner/commands/RegistrationElastixSplineRemoteCommand.java
+++ src/main/java/ch/epfl/biop/atlas/aligner/commands/RegistrationElastixSplineRemoteCommand.java
@@ -7,14 +7,21 @@ import org.scijava.plugin.Plugin;
 @Plugin(type = Command.class, menuPath = "Plugins>BIOP>Atlas>Multi Image To Atlas>Align>Elastix Registration (Spline) on Server")
 public class RegistrationElastixSplineRemoteCommand extends RegistrationCommand {
 
-    @Parameter(label = "Number of control points (along X)", min = "2")
-    int nbControlPointsX;
+    @Parameter(label = "Number of control points along X, minimum 2.", validater = "checkNumberOfPoints")
+    int nbControlPointsX = 10;
 
     @Parameter(label = "Registration Server URL")
     String serverURL;
 
     public void runValidated() {
         mp.registerElastixSplineRemote(serverURL,getFixedFilter(), getMovingFilter(), nbControlPointsX);
+    }
+
+    public void checkNumberOfPoints() {
+        if (nbControlPointsX<2) {
+            mp.errorMessageForUser.accept("Cannot start registration", "Number of control points too low.");
+            validationError = true;
+        }
     }
 
 }
