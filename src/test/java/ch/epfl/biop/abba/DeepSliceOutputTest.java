861c395860b6ec822bb2db9c55e60ff64b096172
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2021-10-14 08:27:37 +0200

    adds deepslice output test

---
+++ src/test/java/ch/epfl/biop/abba/DeepSliceOutputTest.java
@@ -0,0 +1,18 @@
+package ch.epfl.biop.abba;
+
+import ch.epfl.biop.quicknii.QuickNIISeries;
+
+import javax.xml.bind.JAXBContext;
+import java.io.FileReader;
+
+public class DeepSliceOutputTest {
+    public static void main(String... args) throws Exception {
+        // Un marshall xml
+
+        String path = "src/test/resources/quicknii/";
+
+        JAXBContext context = JAXBContext.newInstance(QuickNIISeries.class);
+        QuickNIISeries series = (QuickNIISeries) context.createUnmarshaller()
+                .unmarshal(new FileReader(path+"resultstestfix.xml"));
+    }
+}
