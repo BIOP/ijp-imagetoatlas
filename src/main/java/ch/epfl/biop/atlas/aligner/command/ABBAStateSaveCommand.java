462e635d3d9792fd435a16d30e9c74ee225de168
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2022-02-17 18:14:25 +0100

    fix https://github.com/BIOP/ijp-imagetoatlas/issues/128

--- src/main/java/ch/epfl/biop/atlas/aligner/command/ABBAStateSaveCommand.java
+++ src/main/java/ch/epfl/biop/atlas/aligner/command/ABBAStateSaveCommand.java
@@ -1,6 +1,7 @@
 package ch.epfl.biop.atlas.aligner.command;
 
 import ch.epfl.biop.atlas.aligner.MultiSlicePositioner;
+import org.apache.commons.io.FilenameUtils;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
 import org.scijava.plugin.Parameter;
@@ -16,7 +17,7 @@ public class ABBAStateSaveCommand implements Command {
     @Parameter
     MultiSlicePositioner mp;
 
-    @Parameter(style = "save")
+    @Parameter(style = "save", persist = false)
     File state_file;
 
     @Parameter(persist = false, required = false, type = ItemIO.OUTPUT)
@@ -24,6 +25,28 @@ public class ABBAStateSaveCommand implements Command {
 
     @Override
     public void run() {
-        success = mp.saveState(state_file, true);
+        // Appends extension
+        String extension = FilenameUtils.getExtension(state_file.getAbsolutePath());
+        if ((extension==null)||(extension.trim().equals(""))) {
+            mp.log.accept("Adding json extension to state file");
+            state_file = new File(state_file.getAbsolutePath()+".json");
+        }
+
+        if ((state_file.getAbsolutePath().endsWith("_sources.json"))||(state_file.getAbsolutePath().endsWith("_sources"))) {
+            mp.errlog.accept("Please choose a different file name.");
+            success = false;
+        } else {
+            if (state_file.exists()) {
+               mp.errlog.accept("Error, this file already exists!");
+            } else {
+                String fileNoExt = FilenameUtils.removeExtension(state_file.getAbsolutePath());
+                File sacsFile = new File(fileNoExt+"_sources.json");
+                if (sacsFile.exists()) {
+                    mp.errlog.accept("Error, the file "+sacsFile.getAbsolutePath()+" already exists!");
+                } else {
+                    success = mp.saveState(state_file, true);
+                }
+            }
+        }
     }
 }
