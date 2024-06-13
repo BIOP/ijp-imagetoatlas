66dad82cb33f92d8a5a4cfcfbad719ddaeaa9a6e
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2024-06-13 16:26:43 +0200

    Moves registration module to upstream repo bigdataviewer-biop-tools

--- src/main/java/ch/epfl/biop/registration/plugin/IRegistrationPlugin.java
+++
@@ -1,9 +0,0 @@
-package ch.epfl.biop.registration.plugin;
-
-import bdv.viewer.SourceAndConverter;
-import ch.epfl.biop.registration.Registration;
-import org.scijava.plugin.SciJavaPlugin;
-
-public interface IRegistrationPlugin extends SciJavaPlugin, Registration<SourceAndConverter<?>[]> {
-
-}
