68366d2d19ee146d7eac82f237bda7760ab1bbd4
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2024-08-22 13:10:58 +0200

    Fix thread deadlock in edit Bigwarp

--- src/test/java/ch/epfl/biop/SimpleIJLaunch.java
+++ src/test/java/ch/epfl/biop/SimpleIJLaunch.java
@@ -1,7 +1,9 @@
 package ch.epfl.biop;
 
+import ch.epfl.biop.registration.sourceandconverter.spline.Elastix2DSplineRegistration;
 import loci.common.DebugTools;
 import net.imagej.ImageJ;
+import sc.fiji.bdvpg.scijava.services.SourceAndConverterService;
 
 public class SimpleIJLaunch {
     public static void main(String[] args) {
@@ -11,6 +13,9 @@ public class SimpleIJLaunch {
         final ImageJ ij = new ImageJ();
         //
         ij.ui().showUI();
+
+        Elastix2DSplineRegistration r;
+        SourceAndConverterService s;
         //DebugTools.enableLogging ("DEBUG");
         //RemoteElastixTask.timeOutInMs = 100000;
         //AlphaBlendedResampledSource
